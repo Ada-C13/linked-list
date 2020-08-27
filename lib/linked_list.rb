8ded81b8ce5a6e295cb435e56b7e8c21f42eed04
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-08-26 22:12:23 -0700

    Added functions

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -18,71 +18,148 @@ class LinkedList
 
     # method to add a new node with the specific data value in the linked list
     # insert the new node at the beginning of the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
     def add_first(value)
-      raise NotImplementedError
+        new_node = Node.new(value)
+      if @head.nil?
+        @head = new_node
+      else 
+        new_node.next = @head
+        @head = new_node
+      end
     end
 
     # method to find if the linked list contains a node with specified value
     # returns true if found, false otherwise
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
     def search(value)
-      raise NotImplementedError
+      if @head.nil?
+        return false
+      end
+      current = @head
+      until current.data == value || current.next.nil?
+        current = current.next
+      end
+      return current.data == value
     end
 
     # method to return the max value in the linked list
     # returns the data value and not the node
     def find_max
-      raise NotImplementedError
+      if @head.nil?
+        return nil
+      end
+      current = @head
+      max = @head.data
+      until current.next.nil?
+        current = current.next
+        if current.data > max
+          max = current.data
+        end
+      end
+      return max
     end
 
     # method to return the min value in the linked list
     # returns the data value and not the node
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
     def find_min
-      raise NotImplementedError
+      if @head.nil?
+        return nil
+      end
+      current = @head
+      min = @head.data
+      until current.next.nil?
+        current = current.next
+        if current.data < min
+          min = current.data
+        end
+      end
+      return min
     end
 
 
     # method that returns the length of the singly linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def length
-      raise NotImplementedError
+      if @head.nil?
+        return 0
+      end
+      count = 1
+      current = @head
+      until current.next.nil?
+        current = current.next
+        count += 1
+      end
+      return count
     end
 
     # method that returns the value at a given index in the linked list
     # index count starts at 0
     # returns nil if there are fewer nodes in the linked list than the index value
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def get_at_index(index)
-      raise NotImplementedError
+      if @head.nil?
+        return nil
+      end
+      current = @head
+      count = 0
+      until count == index || current.next.nil?
+        current = current.next
+        count += 1
+      end
+      return current.data
     end
 
     # method to print all the values in the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
     def visit
-      raise NotImplementedError
+      if @head.nil?
+        return nil
+      end
+      current = @head
+      until current.nil?
+        print current.data
+        current = current.next
+      end
     end
 
     # method to delete the first node found with specified value
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
     def delete(value)
-      raise NotImplementedError
+      if @head.nil?
+        return nil
+      end
+      previous = @head
+      current = @head
+      until current.data == value || current.nil?
+        previous = current
+        current = current.next
+      end
+      previous.next == current.next
     end
 
     # method to reverse the singly linked list
     # note: the nodes should be moved and not just the values in the nodes
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def reverse
-      raise NotImplementedError
+      if @head.nil?
+        return nil
+      end
+      current = @head
+      until current.nil?
+        new_previous = current.next
+        new_current = new_previous.next
+        new_previous.next = current
+        current = new_current      
     end
 
 
@@ -115,25 +192,45 @@ class LinkedList
     # Additional Exercises 
     # returns the value in the first node
     # returns nil if the list is empty
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
     def get_first
-      raise NotImplementedError
+      if @head.nil?
+        return nil
+      else
+        return @head.data
+      end
     end
 
     # method that inserts a given value as a new last node in the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def add_last(value)
-      raise NotImplementedError
+      new_node = Node.new(value)
+      if @head.nil?
+        @head = new_node
+      else 
+        current = @head
+        until current.next.nil?
+          current = current.next
+        end
+        current.next = new_node
+      end
     end
 
     # method that returns the value of the last node in the linked list
     # returns nil if the linked list is empty
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
     def get_last
-      raise NotImplementedError
+      if @head.nil?
+        return nil
+      end
+      current = @head
+      while current.next != nil
+        current = current.next
+      end
+      return current.data
     end
 
     # method to insert a new node with specific data value, assuming the linked
@@ -158,4 +255,5 @@ class LinkedList
 
       current.next = @head # make the last node link to first node
     end
+  end
 end
